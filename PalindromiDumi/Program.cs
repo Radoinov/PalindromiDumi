f17340dcc24f0f1ef60f38feb3712326ea3ed626
Radoinov <radoinov08@gmail.com>
2024-03-21 21:44:21 +0200

    Domashna

---
+++ PalindromiDumi/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PalindromiDumi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Napishi duma: ");
+            string word = Console.ReadLine();
+            char[] duma = word.ToCharArray();
+            int mid = duma.Length / 2;
+            int i = 0;
+            int j = duma.Length - 1;
+            bool result = false;
+            if (duma.Length % 2 == 1)
+
+                for (i = 0; i < mid; i++)
+                {
+                    if (duma[i] == duma[duma.Length - 1 - i])
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+            if (result)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
